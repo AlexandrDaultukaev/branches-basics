d3e5f5519be34a7a1924faf97d3016c1262756f7
AlexandrDaultukaev <shyra31102000@mail.ru>
2020-05-01 16:33:24 +0700

    First commit in branch develop

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int main() {
   char string[20];
   printf("What is the commit now?\n");
   scanf("%s", string);
+  if (strcmp(string, "2") == 0) {
+  	strcpy(string, "second");
+  }
   string[0] = toupper(string[0]);
   printf("%s commit\n", string);
   return 0;
