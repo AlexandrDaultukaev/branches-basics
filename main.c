f442bc9560ea31236cfc6fb6a4418145d508932d
AlexandrDaultukaev <shyra31102000@mail.ru>
2020-05-01 16:26:46 +0700

    Second commit

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main() {
-  printf("First commit\n");
+  char string[20];
+  printf("What is the commit now?\n");
+  scanf("%s", string);
+  string[0] = toupper(string[0]);
+  printf("%s commit\n", string);
   return 0;
 }
