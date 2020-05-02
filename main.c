a2730ba1f47d2b0f81de818505cde38d33aa86a4
AlexandrDaultukaev <shyra31102000@mail.ru>
2020-05-02 13:33:00 +0700

    Third master

--- main.c
+++ main.c
@@ -5,6 +5,9 @@ int main() {
   char string[20];
   printf("What is the commit now?\n");
   scanf("%s", string);
+  if (strcmp(string, "3") == 0) {
+  	strcpy(string, "third");
+  }
   string[0] = toupper(string[0]);
   printf("%s commit\n", string);
   return 0;
